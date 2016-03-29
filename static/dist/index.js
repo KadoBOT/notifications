0333950aa3b4d03602169a065bf79821589d159a
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-03-29 20:43:21 -0300

    changed files

--- static/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Hello_1 = require("./components/Hello");
-ReactDOM.render(React.createElement(Hello_1.default, {compiler: "TypeScriptX", framework: "React"}), document.querySelector(".container"));
